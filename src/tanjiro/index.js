3e50470200a5b9209c5ed71b27228faa19c6d932
hiracky16 <h.piiice16@gmail.com>
2020-11-29 12:09:46 +0900

    format 修正

--- src/tanjiro/index.js
+++ src/tanjiro/index.js
@@ -1,39 +1,37 @@
-import p5 from 'p5';
-import 'p5/lib/addons/p5.dom';
+import p5 from 'p5'
+import 'p5/lib/addons/p5.dom'
 
 window.onload = function () {
   const sketch = (p5) => {
-    window.p5 = p5;
-    const height = window.parent.screen.height;
-    const width = window.parent.screen.width;
+    window.p5 = p5
+    const height = window.parent.screen.height
+    const width = window.parent.screen.width
     p5.setup = () => {
       const canvas = p5.createCanvas(width, height)
-      canvas.background(0);
-      canvas.parent('canvas');
-      p5.randomSeed(0);
+      canvas.background(0)
+      canvas.parent('canvas')
     }
 
+    const size = 30
+    const tanjiroGreen = "#25a36e"
     p5.draw = () => {
-      const step = 30;
-      /// 参考: https://game.watch.impress.co.jp/img/gmw/list/1265/178/1.jpg
-      const tanjiroGreen = "#25a37e";
-      for (let y = 0; y <= 800; y += step) {
-        if (y % (step * 2) == 0) {
-          for (let x = step; x <= width; x += step * 2) {
-            p5.noStroke();
-            p5.fill(tanjiroGreen);
-            p5.rect(x, y, step, step);
+      for (let y = 0; y <= height; y += size) {
+        if (y % (size * 2) == 0) {
+          for (let x = size; x <= width; x += size * 2) {
+            p5.noStroke()
+            p5.fill(tanjiroGreen)
+            p5.rect(x, y, size, size)
           }
         } else {
-          for (let x = 0; x <= width; x += step * 2) {
-            p5.noStroke();
-            p5.fill(tanjiroGreen);
-            p5.rect(x, y, step, step);
+          for (let x = 0; x <= width; x += size * 2) {
+            p5.noStroke()
+            p5.fill(tanjiroGreen)
+            p5.rect(x, y, size, size)
           }
         }
       }
     }
   }
 
-  new p5(sketch);
+  new p5(sketch)
 }
